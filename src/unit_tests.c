fe5ad21878fb8c007337cad5c786947e8f2d4e2d
dimi309 <dimi309@users.noreply.github.com>
2021-05-05 08:06:21 +0200

    Initial commit

---
+++ src/unit_tests.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <vkzos.h>
+
+int main(int argc, char **argv) {
+
+  uint32_t num = 0;
+
+  if (vkz_create_instance("title", NULL, num)) {
+    printf("Ok\n\r");
+      
+  }
+  else {
+    printf("Failed\n\r");
+  }
+  return EXIT_SUCCESS;
+}
