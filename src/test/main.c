d6cfaa71ad35078adab1dd6d4965d141ef3c9d12
dimi309 <dimi309@users.noreply.github.com>
2022-02-15 05:07:15 +0100

    Layout undefined issue.

--- src/test/main.c
+++ src/test/main.c
@@ -13,34 +13,30 @@
 
 #ifdef _MSC_VER
 
-VkVertexInputBindingDescription binding_desc[2];
-VkVertexInputAttributeDescription attrib_desc[2];
+#define NUM_FRAMES_IN_FLIGHT 3
+
+VkVertexInputBindingDescription binding_desc[1];
+VkVertexInputAttributeDescription attrib_desc[1];
+VkCommandBuffer command_buffer[NUM_FRAMES_IN_FLIGHT];
+uint32_t current_frame_index = 0;
 
 int set_input_state_callback(VkPipelineVertexInputStateCreateInfo*
   inputStateCreateInfo) {
 
-  memset(binding_desc, 0, 3 * sizeof(VkVertexInputBindingDescription));
+  memset(binding_desc, 0, 1 * sizeof(VkVertexInputBindingDescription));
 
   binding_desc[0].binding = 0;
   binding_desc[0].stride = 4 * sizeof(float);
 
-  binding_desc[1].binding = 1;
-  binding_desc[1].stride = 3;
-
-  memset(attrib_desc, 0, 3 * sizeof(VkVertexInputAttributeDescription));
+  memset(attrib_desc, 0, 1 * sizeof(VkVertexInputAttributeDescription));
 
   attrib_desc[0].binding = 0;
   attrib_desc[0].location = 0;
   attrib_desc[0].format = VK_FORMAT_R32G32B32A32_SFLOAT;
   attrib_desc[0].offset = 0;
 
-  attrib_desc[1].binding = 1;
-  attrib_desc[1].location = 1;
-  attrib_desc[1].format = VK_FORMAT_R8G8B8A8_UINT;
-  attrib_desc[1].offset = 0;
-  
-  inputStateCreateInfo->vertexBindingDescriptionCount = 2;
-  inputStateCreateInfo->vertexAttributeDescriptionCount = 2;
+  inputStateCreateInfo->vertexBindingDescriptionCount = 1;
+  inputStateCreateInfo->vertexAttributeDescriptionCount = 1;
   inputStateCreateInfo->pVertexBindingDescriptions = binding_desc;
   inputStateCreateInfo->pVertexAttributeDescriptions = attrib_desc;
 
@@ -171,7 +167,7 @@ int CALLBACK wWinMain(
     return 1;
   }
 
-  if (!vkz_init(3)) {
+  if (!vkz_init(NUM_FRAMES_IN_FLIGHT)) {
     MessageBox(NULL, "Failed to initialise Vulkan", "Error", MB_OK);
   }
 
@@ -183,45 +179,93 @@ int CALLBACK wWinMain(
     MessageBox(NULL, "Failed to create Vulkan surface", "Error", MB_OK);
   }
 
-  uint32_t pipelineIndex = 100;
+  uint32_t pipeline_index = 100;
 
   if (!vkz_create_pipeline("..\\..\\resources\\shaders\\vertexShader.spv", 
     "..\\..\\resources\\shaders\\fragmentShader.spv",
     set_input_state_callback, set_pipeline_layout_callback,
-    &pipelineIndex)) {
+    &pipeline_index)) {
     MessageBox(NULL, "Failed to create Pipeline", "Error", MB_OK);
   }
-  VkBuffer position_buffer = VK_NULL_HANDLE;
-  VkDeviceMemory position_memory = VK_NULL_HANDLE;
-  if (!vkz_create_buffer(&position_buffer,
+  VkBuffer vertex_buffer = VK_NULL_HANDLE;
+  VkDeviceMemory vertex_buffer_memory = VK_NULL_HANDLE;
+  if (!vkz_create_buffer(&vertex_buffer,
     VK_BUFFER_USAGE_TRANSFER_DST_BIT |
     VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
     16 * sizeof(float),
-    &position_memory,
+    &vertex_buffer_memory,
     VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT)) {
     MessageBox(NULL, "Failed to create postition buffer", "Error", MB_OK);
   }
 
-  VkBuffer stagingBuffer;
-  VkDeviceMemory stagingBufferMemory;
+  VkBuffer staging_buffer;
+  VkDeviceMemory staging_buffer_memory;
 
-  if (vkz_create_buffer(&stagingBuffer,
+  if (vkz_create_buffer(&staging_buffer,
     VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
     16 * sizeof(float),
-    &stagingBufferMemory,
+    &staging_buffer_memory,
     VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
     VK_MEMORY_PROPERTY_HOST_COHERENT_BIT)) {
-    void* stagingData;
-    vkMapMemory(vkz_logical_device, stagingBufferMemory, 0,
+    void* staging_data = NULL;
+    vkMapMemory(vkz_logical_device, staging_buffer_memory, 0,
       VK_WHOLE_SIZE,
-      0, &stagingData);
-    memcpy(stagingData, vertexData, 16 * sizeof(float));
-    vkUnmapMemory(vkz_logical_device, stagingBufferMemory);
+      0, &staging_data);
+    memcpy(staging_data, vertexData, 16 * sizeof(float));
+    vkUnmapMemory(vkz_logical_device, staging_buffer_memory);
 
-    vkz_copy_buffer(stagingBuffer, position_buffer,
+    vkz_copy_buffer(staging_buffer, vertex_buffer,
       16 * sizeof(float));
 
-    vkz_destroy_buffer(stagingBuffer, stagingBufferMemory);
+    vkz_destroy_buffer(staging_buffer, staging_buffer_memory);
+    
+  }
+
+  VkBuffer index_buffer = VK_NULL_HANDLE;
+  VkDeviceMemory index_buffer_memory = VK_NULL_HANDLE;
+
+  if (!vkz_create_buffer(&index_buffer,
+    VK_BUFFER_USAGE_TRANSFER_DST_BIT |
+    VK_BUFFER_USAGE_INDEX_BUFFER_BIT,
+    6 * sizeof(uint32_t),
+    &index_buffer_memory,
+    VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT)) {
+    MessageBox(NULL, "Failed to create index buffer", "Error", MB_OK);
+  }
+
+  if (vkz_create_buffer(&staging_buffer,
+    VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+    6 * sizeof(uint32_t),
+    &staging_buffer_memory,
+    VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
+    VK_MEMORY_PROPERTY_HOST_COHERENT_BIT)) {
+    void* staging_data;
+    vkMapMemory(vkz_logical_device, staging_buffer_memory, 0,
+      VK_WHOLE_SIZE,
+      0, &staging_data);
+    memcpy(staging_data, indexData, 6 * sizeof(uint32_t));
+    vkUnmapMemory(vkz_logical_device, staging_buffer_memory);
+
+    vkz_copy_buffer(staging_buffer, index_buffer,
+      6 * sizeof(uint32_t));
+
+    vkz_destroy_buffer(staging_buffer, staging_buffer_memory);
+  }
+  else {
+    MessageBox(NULL, "Failed to create staging buffer for indices", "Error", MB_OK);
+  }
+
+
+  for (int idx = 0; idx < NUM_FRAMES_IN_FLIGHT; ++idx) {
+    command_buffer[idx] = VK_NULL_HANDLE;
+    
+    vkz_begin_draw_command_buffer(&command_buffer[idx]);
+    vkz_bind_pipeline_to_command_buffer(pipeline_index, &command_buffer[idx]);
+    VkDeviceSize binding = 0;
+    vkCmdBindVertexBuffers(command_buffer[idx], 0, 1, &vertex_buffer, &binding);
+    vkCmdBindIndexBuffer(command_buffer[idx], index_buffer, 0, VK_INDEX_TYPE_UINT32);
+    vkCmdDrawIndexed(command_buffer[idx], 4, 1, 0, 0, 0);
+    vkz_end_draw_command_buffer(&command_buffer[idx]);
   }
 
   ShowWindow(hWnd, SW_SHOW);
@@ -231,13 +275,26 @@ int CALLBACK wWinMain(
 
   while (msg.message != WM_QUIT)
   {
+    int image_index = 0;
+    vkz_acquire_next_image(pipeline_index, &image_index, &current_frame_index);
+    vkz_wait_gpu_cpu_fence(current_frame_index);
+    vkz_draw(&command_buffer[current_frame_index]);
+    vkz_present_next_image();
+
     if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
     {
       TranslateMessage(&msg);
       DispatchMessage(&msg);
     }
   }
-  vkz_destroy_pipeline(pipelineIndex);
+
+  for (int idx = 0; idx < NUM_FRAMES_IN_FLIGHT; ++idx) {
+    vkz_wait_gpu_cpu_fence(idx);
+    vkz_destroy_draw_command_buffer(&command_buffer[idx]);
+  }
+
+  vkz_destroy_buffer(vertex_buffer, vertex_buffer_memory);
+  vkz_destroy_pipeline(pipeline_index);
   vkz_destroy_swapchain();
   vkz_destroy_sync_objects();
   vkz_shutdown();
