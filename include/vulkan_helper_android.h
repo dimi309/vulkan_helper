550c06ab35fdd185b25aa5bcf5ca4d20c90a388e
dimi309 <dimi309@users.noreply.github.com>
2023-03-23 05:19:40 +0100

    Removed vulkan_helper_android.h BSD license.

--- include/vulkan_helper_android.h
+++ include/vulkan_helper_android.h
@@ -1,12 +1,3 @@
-/**
- * @file small3d_android.h
- * @brief Include file for Android builds
- *
- * Created on: 2022/09/22
- *     Author: Dimitri Kourkoulis
- *     License: BSD 3-Clause License (see LICENSE file)
- */
-
 #include <android_native_app_glue.h>
 extern struct android_app *vh_android_app;
 
